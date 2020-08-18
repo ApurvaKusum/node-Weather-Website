0e635f4de095fdb977e1d08dbf2b93917569b2d9
APURVA KUSUM <apurva.r.26@gmail.com>
2020-08-18 22:34:41 +0530

    dding extra data to weather page

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + ' It is currently ' + body.current.temperature + ' degress out. It feel ' + body.current.feelslike + 'degree outside.')
+            callback(undefined,'It is '+ body.current.weather_descriptions[0] + ' outside. It is currently ' + body.current.temperature + ' degress out. It feel ' + body.current.feelslike + 'degree outside.'+'Outside humidity is '+body.current.humidity+'.The visibility outside is '+body.current.visibility+'%')
         }
     })
 }
